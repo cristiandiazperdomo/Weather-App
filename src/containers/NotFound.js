8d4dbcc2885d904b5071ad32381e8979c20d8709
unknown <danubiodiaz2003edf@gmail.com>
2022-03-25 20:01:23 -0300

    NotFound Page

---
+++ src/containers/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/components/NotFound.css';
+
+export const NotFound = () => {
+
+	return (
+		<div className="NotFound">
+		<img src={require('../img/404error.png')}/>
+			<Link to="/">
+				<button type="button">Come to the main page</button>
+			</Link>
+		</div>
+	)
+
+}
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Home } from '../containers/Home';
+import { NotFound } from '../containers/NotFound';
 import { Layout } from '../components/Layout';
 import { MyContext } from '../context/AppContext';
 import { ShowWeather } from '../components/ShowWeather';
@@ -16,8 +17,9 @@ export const App = () => {
 			 <Router>
 				<Layout>
 					<Routes>
+						<Route path="*" element={<NotFound/>} />
 						<Route exact path="/" element={<Home />} />
-						<Route exact path="/weather" element={initialState.weatherApiInfo.length === 0 ?  <Navigate to="/"/> : <ShowWeather />} />
+						<Route exact path="/weather" element={initialState.weatherApiInfo.length === 0 ?  <Navigate to="/" /> : <ShowWeather />} />
 					</Routes>
 				</Layout>
 			</Router>
